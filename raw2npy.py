3eb88c7c1546bc3aacacc8dcfc7c314d68ebc858
unknown <jardel@ufc.br>
2018-07-17 00:54:18 -0300

    vdc

---
+++ raw2npy.py
@@ -0,0 +1,18 @@
+
+
+import numpy as np
+from scipy.misc import imread
+from matplotlib import pyplot as ppl
+import os
+import sys
+from scipy.misc import imsave
+from mayavi import mlab
+import matplotlib.pyplot as ppl
+
+x=np.fromfile("saida.raw", dtype='int', sep="")
+x = x.reshape([205, 281,420])
+
+np.save("saida.npy",x)
+
+#A = np.fromfile(filename, dtype='int16', sep="")
+
